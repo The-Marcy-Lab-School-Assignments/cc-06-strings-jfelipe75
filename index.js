5e6025656685c79ca424cca72fa8bcb77af798e6
Juan Felipe Garcia <jfelipegarcia23@gmail.com>
2024-10-10 12:25:08 -0400

    Completed code challenege

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 //write your code here
+const reverseString = (str) => {
+    let newStr = '';
+    for (let i = str.length -1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+}
+
+console.log(reverseString('hello'));
+
+
+const reverseZigZagString = (str) => {
+    let newStr = '';
+    for (let i =str.length - 1; i >= 0; i--) {
+        if (i % 2 === 0) {
+            newStr += str[i];
+           
+        }  else {
+            newStr += str[i].toUpperCase();
+        }
+    }
+    return newStr;
+};
+
+console.log(reverseZigZagString('hello'));
